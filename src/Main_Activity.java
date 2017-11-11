2edea7baeb240a706f56c1c76675d242b351dc46
adsingh <ads7194@gmail.com>
2017-11-11 17:15:50 -0500

    cleaned up code

--- src/Main_Activity.java
+++ src/Main_Activity.java
@@ -25,7 +25,6 @@ public class Main_Activity {
 					String[] cmdParts = line.split("\\(");
 					String cmd = cmdParts[0].toLowerCase();
 					String[] params = cmdParts[1].split(",");
-					//System.out.println(params.length);
 					
 					switch(cmd){
 					case "insert":
@@ -49,37 +48,6 @@ public class Main_Activity {
 		catch(IOException e){
 			e.printStackTrace();
 		}
-		//bPlusTree.printTree(bPlusTree.root);
-		bPlusTree.search(100, 400);
-//		BPlus_Tree bT = new BPlus_Tree(3);
-//		bT.insert(5,"5A");
-//		bT.insert(3,"3A");
-//		bT.insert(8,"8A");
-//		bT.insert(5, "5B");
-//		bT.insert(2,"2A");
-//		bT.insert(3,"3B");
-//		bT.insert(4,"4A");
-//		bT.insert(1,"1A");
-//		bT.insert(0,"0A");
-//		bT.insert(-1,"-1A");
-//		bT.insert(6,"6A");
-//		bT.insert(7,"7A");
-//		bT.insert(7, "7B");
-//		bT.insert(6, "6B");
-//
-//		bT.insert(17,"17A");
-//		bT.insert(27, "27A");
-//		bT.insert(60, "60A");
-//		
-//		bT.insert(12, "12A");
-//		bT.insert(15, "15A");
-//        bT.search(17);
-//        bT.insert(17,"17B");
-//        bT.search(60);
-//        //bT.printTree(bT.root);
-//        bT.search(2, 100);
-//        
-//		System.out.println("");
 	}
 
 }
